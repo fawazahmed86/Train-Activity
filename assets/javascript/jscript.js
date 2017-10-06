39bd2c78b1bc99846ea8057765640992c7c1af31
fawazahmed86 <honorfas@gmail.com>
2017-10-05 21:19:08 -0700

    updated files with minor css modifications

--- assets/javascript/jscript.js
+++ assets/javascript/jscript.js
@@ -1,4 +1,4 @@
-  // Initialize Firebase
+
   var config = {
     apiKey: "AIzaSyDmBwkwy1qChpXgrJZ_d-7BQ6H4DEZdLsQ",
     authDomain: "project1-3bbd1.firebaseapp.com",
@@ -27,27 +27,22 @@
 
     
 
-    // First Time (pushed back 1 year to make sure it comes before current time)
+  
     var timeConverted = moment(time, "hh:mm").subtract(1, "years");
     console.log(timeConverted);
 
-    // Current Time
     var currentTime = moment();
     console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
 
-    // Difference between the times
     var diffTime = moment().diff(moment(timeConverted), "minutes");
     console.log("DIFFERENCE IN TIME: " + diffTime);
 
-    // Time apart (remainder)
     var tRemainder = diffTime % freq;
     console.log(tRemainder);
 
-    // Minute Until Train
     var tMinutesTillTrain = freq - tRemainder;
     console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
 
-    // Next Train
     var nextTrain = moment().add(tMinutesTillTrain, "minutes");
     console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
 
